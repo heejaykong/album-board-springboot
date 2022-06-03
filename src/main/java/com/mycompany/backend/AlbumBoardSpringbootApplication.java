14e1c98a3de2f8c193e6a72ed4988357f9576974
heejaykong <heejay.dev@gmail.com>
2022-06-03 10:31:50 +0900

    init

---
+++ src/main/java/com/mycompany/backend/AlbumBoardSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumBoardSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumBoardSpringbootApplication.class, args);
+	}
+
+}
